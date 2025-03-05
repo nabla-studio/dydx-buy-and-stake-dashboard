58c648190b6d49eeb048982d3987d9d2a87b7286
Davide Segullo <davidesegullo@gmail.com>
2025-03-05 13:53:38 +0100

    feat: :lipstick: add basic layout

---
+++ components/navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export const Navbar = () => <nav className="container py-5 flex items-center justify-between">
+<Link href="/">Logo</Link>
+
+<Button asChild>
+  <Link href="https://dydx.trade/?utm_source=dydx-buy-and-stake-dashboard">
+  Trade
+  </Link>
+</Button>
+</nav>
