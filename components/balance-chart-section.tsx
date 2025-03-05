31a6c62ee1ceb3fdd52100fae669133c54f8a245
Davide Segullo <davidesegullo@gmail.com>
2025-03-05 22:53:48 +0100

    fix: :bug: fix ssr error

--- components/balance-chart-section.tsx
+++ components/balance-chart-section.tsx
@@ -148,7 +148,7 @@ export function BalanceChartSection({
                   {chartConfig[chart].label}
                 </span>
                 <span className="text-lg font-bold leading-none sm:text-3xl">
-                  {total[key as keyof typeof total].toLocaleString()}
+                  {total[key as keyof typeof total]}
                 </span>
               </button>
             );
