ae2fbb43077b499d9282ccdeb9507dc08c6d3390
Davide Segullo <davidesegullo@gmail.com>
2025-04-01 23:47:38 +0200

    feat: :lipstick: improvement after design review

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,15 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 
 export const Navbar = () => (
-  <nav className="px-5 xl:px-10 2xl:container py-5 flex items-center justify-between">
-    <Link href="/">
+  <nav className="px-5 xl:px-10 2xl:container pb-5 pt-10 flex items-center justify-between">
+    <Link className="flex items-center gap-4" href="/">
       <Image alt="dYdX logo" src="/logo.svg" width={100} height={32} />
+
+      <span className="text-[10px] text-primary font-bold uppercase leading-tight">
+        Buyback
+        <br />
+        program
+      </span>
     </Link>
 
     <Button asChild>
