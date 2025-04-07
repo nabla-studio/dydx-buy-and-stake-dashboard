23ab6274fef4d9a488c3afeff664d19362daf171
Davide Segullo <davidesegullo@gmail.com>
2025-04-07 12:10:30 +0200

    feat: :lipstick: add style for disabled next buyback amount

--- components/next-amount-card.tsx
+++ components/next-amount-card.tsx
@@ -1,21 +1,30 @@
 "use client";
 
+import { useDateFilter } from "@/hooks/date-filter";
 import { nextBuybackAmountQuery } from "@/queries/options";
 import { useQuery } from "@tanstack/react-query";
 import type { ComponentProps } from "react";
 import { GenericCard } from "./generic-card";
 
 export function NextAmountCard({ ...rest }: ComponentProps<"div">) {
+  const { notDefaultValue } = useDateFilter();
   const { data } = useQuery(nextBuybackAmountQuery);
+
   return (
     <GenericCard
       title="Next Buyback Amount"
       description="Real-time estimate based on USDC in Buyback wallet."
       {...rest}
     >
-      <div className="flex flex-col items-center gap-1">
+      <div className="relative flex flex-col items-center gap-1">
         <h3 className="text-foreground text-7xl font-bold">{data}</h3>
         <p className="text-primary text-sm">DYDX</p>
+
+        {notDefaultValue ? (
+          <div className="absolute inset-0 bg-background text-base flex items-center justify-center text-primary font-bold">
+            We can’t calculate
+          </div>
+        ) : null}
       </div>
     </GenericCard>
   );
