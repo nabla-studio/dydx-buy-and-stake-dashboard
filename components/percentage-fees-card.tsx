acd9b318f3bfc931347eea81ee54889775c99918
Giorgio Nocera <giorgio.nocera@nabla.studio>
2025-04-15 09:31:10 +0200

    revert: :rewind: revert to previous version
    Reverting the code to the previous version including dynamic data for percentage fees.

--- components/percentage-fees-card.tsx
+++ components/percentage-fees-card.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useDateFilter } from "@/hooks/date-filter";
+import { buybackFeeShareQuery } from "@/queries/options";
+import { useQuery } from "@tanstack/react-query";
 import type { ComponentProps } from "react";
 import { GenericCard } from "./generic-card";
 
 export function PercentageFeesCard({ ...rest }: ComponentProps<"div">) {
+  const { dates, notDefaultValue } = useDateFilter();
+  const { data, isError } = useQuery(
+    buybackFeeShareQuery(dates.from, dates.to),
+  );
+
   return (
     <GenericCard
       title="Fees Allocated to Buybacks"
@@ -9,7 +19,29 @@ export function PercentageFeesCard({ ...rest }: ComponentProps<"div">) {
       {...rest}
     >
       <div className="relative w-full flex flex-col items-center gap-1">
-        <h3 className="text-foreground text-7xl font-bold">25%</h3>
+        <h3 className="text-foreground text-7xl font-bold">
+          {!data?.first && !data?.last ? (
+            "N/A"
+          ) : notDefaultValue ? (
+            data.first !== data.last ? (
+              <span className="flex flex-col items-center">
+                {data.first}
+                <span>-</span>
+                {data.last}
+              </span>
+            ) : (
+              "0"
+            )
+          ) : (
+            data.last
+          )}
+        </h3>
+
+        {isError ? (
+          <div className="absolute inset-0 bg-background text-base flex items-center justify-center text-primary font-bold text-center">
+            Something went wrong
+          </div>
+        ) : null}
       </div>
     </GenericCard>
   );
